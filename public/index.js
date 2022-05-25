96eab609703005aa6da4bb857d13970640f5904f
George Merida <gmerida92@gmail.com>
2022-05-24 21:03:45 -0700

    Second

--- public/index.js
+++ public/index.js
@@ -1,6 +1,23 @@
+const changeCat = async () => {
+    let fetched = await fetch('https://api.thecatapi.com/v1/images/search',{
+        method:'GET',
+        headers:{"x-api-key" : "{$$.env.x-api-key}"}
+    })
+
+
+
+    let body = await fetched.json();
+
+    let url = body[0].url;
+
+    // console.log(url)
+
+    return url;
+}
+
 
 window.onload = () =>{
-
+// Phase 1
     fetch('https://api.thecatapi.com/v1/images/search',{
         method:'GET',
         headers:{"x-api-key" : "{$$.env.x-api-key}"}
@@ -27,6 +44,52 @@ window.onload = () =>{
         container.style.flexDirection = 'column'
         container.style.alignItems = 'center'
         document.body.appendChild(container);
+
+// Phase 2
+        let changeButton = document.createElement("button");
+        changeButton.innerText = "New Cat"
+        container.appendChild(changeButton);
+
+        const votes = {
+            upvote: 0,
+            downvote: 0,
+        }
+
+        let picPop = document.createElement("div")
+        picPop.innerText = `Popularity Score: ${votes.upvote - votes.downvote}`;
+        container.appendChild(picPop);
+
+        let upvoteButton = document.createElement("button");
+        let downvoteButton = document.createElement("button");
+
+        upvoteButton.innerText = "Upvote";
+        downvoteButton.innerText = "Downvote"
+
+        container.appendChild(upvoteButton);
+        container.appendChild(downvoteButton);
+
+        changeButton.addEventListener("click", event => {
+            changeCat().then(res => imgElement.setAttribute("src", res));
+            votes.upvote = 0;
+            votes.downvote = 0;
+            picPop.innerText = `Popularity Score: ${votes.upvote - votes.downvote}`;
+            // console.log(imgElement);
+        });
+
+        upvoteButton.addEventListener("click", event => {
+            votes.upvote++;
+            picPop.innerText = `Popularity Score: ${votes.upvote - votes.downvote}`;
+            event.preventDefault();
+        })
+
+        downvoteButton.addEventListener("click", event => {
+            votes.downvote++;
+            picPop.innerText = `Popularity Score: ${votes.upvote - votes.downvote}`;
+            event.preventDefault();
+        })
+
+
+
 
     })
 
